9cab9d30418eedacb87a0907df762364b136c3f4
chetas11 <cbirmole@gmail.com>
2020-10-12 22:34:19 +0530

    added more cards

--- index.js
+++ index.js
@@ -18,53 +18,22 @@ const Col3 = document.createElement("div");
 Col3.classList.add("col-lg-4","col-sm-6")
 Row.append(Col3)
 
-const Card = document.createElement("div");
-Card.classList.add("card", "text-center", "mt-5")
-Col1.append(Card)
+function createRow() {
+    let Row = document.createElement("div");
+    Row.classList.add("row")
+    return Row
+}
 
-const CardBody = document.createElement("div");
-CardBody.classList.add("card-body")
-Card.append(CardBody)
+function createColumns(){
+    let Col = document.createElement("div");
+    Col.classList.add("col-lg-4","col-sm-6")
+    return Col
+}
 
-const CardTitle = document.createElement("h5");
-CardTitle.innerText = "Card title"
-CardTitle.classList.add("card-body")
-CardBody.append(CardTitle)
-
-const CardImg = document.createElement("img");
-CardImg.src = ""
-CardImg.classList.add("card-body")
-CardBody.append(CardImg)
-
-const Capital = document.createElement("p");
-Capital.innerText = "Some"
-Capital.classList.add("card-text")
-CardBody.append(Capital)
-
-const Region = document.createElement("p");
-Region.innerText = "Some"
-Region.classList.add("card-text")
-CardBody.append(Region)
-
-const CountryCode = document.createElement("p");
-CountryCode.innerText = "Some"
-CountryCode.classList.add("card-text")
-CardBody.append(CountryCode)
-
-const LatLan = document.createElement("p");
-LatLan.innerText = "Some"
-LatLan.classList.add("card-text")
-CardBody.append(LatLan)
-
-const Weather = document.createElement("p");
-Weather.innerText = ""
-Weather.classList.add("card-text", "weather")
-CardBody.append(Weather)
-
-const WeatherBtn = document.createElement("button");
-WeatherBtn.innerText = "Click for weather"
-WeatherBtn.classList.add("btn", "btn-primary")
-CardBody.append(WeatherBtn)
+Row2 = createRow();
+appendElement(Container,Row2)
+Row2Col1 = createColumns()
+appendElement(Row2,Row2Col1)
 
 
 
@@ -84,21 +53,112 @@ fetchData("https://restcountries.eu/rest/v2/all")
     let randomNum = Math.floor(Math.random()*250);
     // console.log(randomNum)
 
-    CardTitle.innerText = data[randomNum].name
-    Capital.innerText = "Capital:"+data[randomNum].capital
-    Region.innerText = "Region:"+data[randomNum].region
-    CountryCode.innerText = "Country Code:"+data[randomNum].cioc
-    LatLan.innerText = "Latitude and Langitude: "+data[randomNum].latlng[0]+", "+data[randomNum].latlng[1]
-    CardImg.src =  data[randomNum].flag
+     let CardTitle = document.querySelectorAll("h5")
+     let Capital = document.querySelectorAll(".capital")
+     let Region = document.querySelectorAll(".region")
+     let CountryCode = document.querySelectorAll(".code")
+     let LatLan = document.querySelectorAll(".latlan")
+     let CardImg = document.querySelectorAll("img")
+     let WeatherBtn = document.querySelectorAll(".btn")
+     let Weather = document.querySelectorAll(".weather")
 
-    WeatherBtn.addEventListener("click",function(){
-    fetch("https://api.openweathermap.org/data/2.5/weather?lat="+data[randomNum].latlng[0]+"&lon="+data[randomNum].latlng[1]+"&appid=apikey").then(res => {
-     return res.json();
-}).then(function(res) {
-    Weather.innerText = (res.weather[0].description.toUpperCase());
-});
+     console.log(Weather)
+
+    
+     
+    for(i=0; i<4; i++){
+        
+    let randomNum = Math.floor(Math.random()*250);
+
+    CardTitle[i].innerText = data[randomNum].name
+    Capital[i].innerText = "Capital:"+data[randomNum].capital
+    Region[i].innerText = "Region:"+data[randomNum].region
+    CountryCode[i].innerText = "Country Code:"+data[randomNum].cioc
+    LatLan[i].innerText = "Latitude and Langitude: "+data[randomNum].latlng[0]+", "+data[randomNum].latlng[1]
+    CardImg[i].src =  data[randomNum].flag
+    
+    WeatherBtn[i].addEventListener("click",function(){
+        fetch("https://api.openweathermap.org/data/2.5/weather?lat="+data[randomNum].latlng[0]+"&lon="+data[randomNum].latlng[1]+"&appid=APIKEY").then(res => {
+        return res.json();
+    }).then(function(res) {
+        Weather[i].innerText = (res.weather[0].description);
+
+        console.log(res.weather[0].description);
+
+    });
 })
+
+    }
 })
+
+
+
+function makeCard(){
+    let cardName = document.createElement("div");
+    cardName.classList.add("card", "text-center")
+  
+
+    let cardBody = document.createElement("div");
+    cardBody.classList.add("card-body", "text-center")
+    cardName.append(cardBody)
+
+    let CardTitle = document.createElement("h5");
+    CardTitle.innerText = "Card title"
+    cardBody.append(CardTitle)
+
+    let CardImg = document.createElement("img");
+    CardImg.src = ""
+    CardImg.classList.add("card-body")
+    cardBody.append(CardImg)
+
+    let Capital = document.createElement("p");
+    Capital.innerText = ""
+    Capital.classList.add("card-text","capital")
+    cardBody.append(Capital)
+
+    let Region = document.createElement("p");
+    Region.innerText = ""
+    Region.classList.add("card-text" , "region")
+    cardBody.append(Region)
+
+    let CountryCode = document.createElement("p");
+    CountryCode.innerText = ""
+    CountryCode.classList.add("card-text" , "code")
+    cardBody.append(CountryCode)
+
+    let LatLan = document.createElement("p");
+    LatLan.innerText = ""
+    LatLan.classList.add("card-text","latlan")
+    cardBody.append(LatLan)
+
+    let Weather = document.createElement("p");
+    Weather.innerText = ""
+    Weather.classList.add("card-text", "weather")
+    cardBody.append(Weather)
+
+    let WeatherBtn = document.createElement("button");
+    WeatherBtn.innerText = "Click for weather"
+    WeatherBtn.classList.add("btn", "btn-primary")
+    cardBody.append(WeatherBtn)
+
+    return cardName
+}
+
+function appendElement(parent, cardName){
+    parent.append(cardName)
+}
+
+Card1 = makeCard()
+appendElement(Col1, Card1);
+Card2 = makeCard()
+appendElement(Col2, Card2);
+Card3 = makeCard()
+appendElement(Col3, Card3);
+Card4 = makeCard()
+appendElement(Row2Col1, Card4);
+
+
+
 
 
 
